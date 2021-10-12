b2d140ffdff13626a922a6331253546441a7a43c
Christopher Valle <chrisbv@live.com>
2021-10-12 16:20:43 -0500

    commit tree d06b29263ab572097f9c396a05a6882f3dd7ee70

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+const num1 = 2;
+const num2 = 31;
+const multiply = num1 * num2;
 
-// Write your code here
+let random = Math.floor(Math.random() * 10) + 1;
+
+let num3 = 1;
+let num4 = 4;
+let mod = num4 / num3;
+
+let max = Math.max(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20);
